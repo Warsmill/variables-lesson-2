4491179f1866a2200b3157b6cac0f3555dfd9c1d
warsmill <warsmill@yandex.ru>
2025-03-13 18:00:36 +0300

    declaration of variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        //задание 1
+        byte number1 = 127;
+        short number2 = 128;
+        int number3 = 33_000;
+        long number4 = 1_000_000_000_000L;
+        float number5 = 1.000001f;
+        double number6 = 1.0000000000001;
     }
 }
